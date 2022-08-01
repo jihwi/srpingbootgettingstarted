59fe6eb9f7e882c504d7f0f47e0d4488652c1961
김지휘 <jihwi.kim@cj.net>
2022-08-01 11:30:39 +0900

    2022-08-01 https, http2 등 내장 톰캣 custom

--- src/main/java/com/jihwi/Application.java
+++ src/main/java/com/jihwi/Application.java
@@ -1,15 +1,79 @@
 package com.jihwi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication springApplication = new SpringApplication(Application.class);
+//        springApplication.setWebApplicationType(WebApplicationType.NONE);
+//        springApplication.run(args);
+////        SpringApplication.run(Application.class, args);
+//    }
+
+    //override되지 않고, 내가 등록한 빈 보다 autoConfiguration이 더 우선된다.
+//    @Bean
+//    public Holoman holoman(){
+//        Holoman holoman = new Holoman();
+//        holoman.setName("whiteship");
+//        holoman.setHowLong(60);
+//        return holoman;
+//    }
+
+    //tocmat 설정 해보기
+//    public static void main(String[] args) throws LifecycleException {
+//        Tomcat tomcat = new Tomcat();
+//        tomcat.setPort(8090);
+//        //context path prefix개념
+//        //어플리케이션 구분을 위해 사용됨 localhost:[port]/[projectName]/[servlet url]
+//        Context context = tomcat.addContext("/tomcat", "/");
+//        HttpServlet servlet = new HttpServlet() {
+//            @Override
+//            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                PrintWriter writer = resp.getWriter();
+//                writer.println("<html><head><title>");
+//                writer.println("hey, tomcat");
+//                writer.println("</title></head>");
+//                writer.println("<body><h1> hello, tomcat </h1></body>");
+//                writer.println("</html>");
+//            }
+//        };
+//
+//        String servletName = "helloServlet";
+//        //context path prefix개념
+//        tomcat.addServlet("/tomcat", servletName, servlet);
+//        //mapping url
+//        context.addServletMappingDecoded("/hello", servletName);
+//
+//
+//        tomcat.start();
+//        tomcat.getServer().await();
+//    }
+
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
-        springApplication.run(args);
-//        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello, spring";
+    }
+
+    //https설정이후 http로도 받고 싶으면 다른 포트에 conector하나 더 만들기
+//    @Bean
+//    public ServletWebServerFactory serverFactory(){
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8090);
+//        return connector;
+//    }
 }
---
+++ src/main/java/com/jihwi/PortListener.java
@@ -0,0 +1,19 @@
+package com.jihwi;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 서블릿이 만들어지는 이벤트 콜백
+ * 빈으로만 들로해주면 다른 설정필요없이 콜백 함수로 하기 메소드가 실행된다.
+ */
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+        System.out.println(applicationContext.getWebServer().getPort());
+    }
+}
