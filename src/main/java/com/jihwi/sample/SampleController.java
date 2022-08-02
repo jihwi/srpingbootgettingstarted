6d701ed0c5a179e74c0b656a04f258d15096be95
김지휘 <jihwi.kim@cj.net>
2022-08-02 16:54:51 +0900

    2022-08-02 test (outputCapture)

--- src/main/java/com/jihwi/sample/SampleController.java
+++ src/main/java/com/jihwi/sample/SampleController.java
@@ -1,5 +1,7 @@
 package com.jihwi.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    private Logger logger = LoggerFactory.getLogger(SampleController.class);
     @Autowired
     private SampleSerivce sampleSerivce;
 
     @GetMapping("/hello")
     public String hello(){
+        logger.debug("log 남기기");
+        System.out.println("이것도 남는지 테스트");
         return "hello " + sampleSerivce.getName();
     }
 }
--- src/test/java/com/jihwi/test/WebClientTest.java
+++ src/test/java/com/jihwi/test/WebClientTest.java
@@ -1,11 +1,15 @@
 package com.jihwi.test;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,11 +18,19 @@ public class WebClientTest {
     @Autowired
     WebTestClient webTestClient;
 
+    //로그 검사할때 log capture해서 assertThat
+    //근데 로그를 검사할일 거의 없어서 사용안할듯.
+    @Rule //junit의 애너테이션
+    public OutputCapture outputCapture = new OutputCapture(); //필수적으로 public 이여야함.
+
     @Test
     public void test(){
         webTestClient.get().uri("/hello").exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
                 .isEqualTo("hello jihwi");
+
+        assertThat(outputCapture.toString()).contains("log")
+                .contains("테스트");
     }
 }
